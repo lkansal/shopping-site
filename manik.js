a9e0bdb15d9bb554a3d7bd913ac19319c757f185
lkansal <44518081+lkansal@users.noreply.github.com>
2020-08-29 19:50:47 +0530

    added files

---
+++ manik.js
@@ -0,0 +1,140 @@
+function a()
+{
+		
+document.getElementById("o").style.fontSize="50px";
+
+}
+	
+function b()
+
+{
+	
+document.getElementById("o").style.fontSize="20px";
+
+}
+function a1()
+{
+		
+document.getElementById("o1").style.fontSize="50px";
+
+}
+	
+function b1()
+
+{
+	
+document.getElementById("o1").style.fontSize="20px";
+
+}
+function a2()
+{
+		
+document.getElementById("o2").style.fontSize="50px";
+		
+}
+	
+function b2()
+
+{
+	
+document.getElementById("o2").style.fontSize="20px";
+
+}
+function a3()
+{
+		
+document.getElementById("o3").style.fontSize="50px";
+		
+}
+	
+function b3()
+
+{
+	
+document.getElementById("o3").style.fontSize="20px";
+
+}
+function a4()
+{
+		
+document.getElementById("o4").style.fontSize="50px";
+		
+}
+	
+function b4()
+
+{
+	
+document.getElementById("o4").style.fontSize="20px";
+
+}
+function a5()
+{
+		
+document.getElementById("o5").style.fontSize="50px";
+		
+}
+	
+function b5()
+
+{
+	
+document.getElementById("o5").style.fontSize="20px";
+
+}
+function a6()
+{
+		
+document.getElementById("o6").style.fontSize="50px";
+		
+}
+	
+function b6()
+
+{
+	
+document.getElementById("o6").style.fontSize="20px";
+
+}
+function a7()
+{
+		
+document.getElementById("o7").style.fontSize="50px";
+		
+}
+	
+function b7()
+
+{
+	
+document.getElementById("o7").style.fontSize="20px";
+
+}
+function a8()
+{
+		
+document.getElementById("o8").style.fontSize="50px";
+		
+}
+	
+function b8()
+
+{
+	
+document.getElementById("o8").style.fontSize="20px";
+
+}
+function a9()
+{
+		
+document.getElementById("o9").style.fontSize="50px";
+		
+}
+	
+function b9()
+
+{
+	
+document.getElementById("o9").style.fontSize="20px";
+
+}
